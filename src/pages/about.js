fcde025ed3c0b11fb9816b246075d293d1629c72
Tomas Carignano <tomascarignano98@gmail.com>
2021-10-20 10:18:55 -0300

    Pages and routes.

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function About() {
+  return (
+    <main>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, ullam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, ullam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, ullam.
+      </p>
+    </main>
+  )
+}
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
-import React from "react"
+import React from "react";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section>
+      <h1>Design</h1>
+      <h2>Develop & Deploy</h2>
+      <p>UX designer & and web devloper based in Manchester.</p>
+    </section>
+  );
 }
