5de8d5fca2fa52ef6654a6750f42c3098ec5083b
Tomas Carignano <tomascarignano98@gmail.com>
2021-10-25 11:14:34 -0300

    Query Variables.

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,20 +1,45 @@
 import React from "react"
-// import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 import Layout from "../components/Layout"
 import * as styles from "../styles/project-details.module.css"
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+  const { html } = data.markdownRemark
+  const { title, stack, slug, featured } = data.markdownRemark.frontmatter
+
   return (
     <Layout>
       <div className={styles.details}>
-        <h1>Title</h1>
-        <h3>stack</h3>
+        <h1>{title}</h1>
+        <h3>{stack}</h3>
         <div className={styles.featured}>
-          {/* <GatsbyImage image={getImage} /> */}
+          <GatsbyImage image={getImage(featured)} />
         </div>
-        {/* <div className={styles.html} dangerouslySetInnerHTML={}/> */}
+        <div
+          className={styles.html}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ProjectsDetails($slug: String = "") {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        slug
+        stack
+        title
+        featured {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  }
+`
